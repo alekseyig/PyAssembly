0f93e42b35a78c18ad1bcd4c20a1d71dd0bb575d
Aleksey Zhukov <azhukov@grubhub.com>
2018-01-31 22:38:55 -0600

    Clean up.

---
+++ README.md
@@ -0,0 +1,36 @@
+PyAssembly
+===============================
+Pyassembly is the Python setup.py command. If you are familiar with fat or uber jar, than this command will come as no surprise for you. By installing and using it, you will get an "uber" egg or zip file, which will contain not only your project, but will also include all the dependencies you specified in you requirements.txt file, including the dependence of the dependencies (a.k.a. transitive dependencies), making it easy to create a self-contained distribution.
+
+
+Installing
+----------
+
+.. code-block:: shell
+
+	$ pip install pyassembly
+	
+Alternatively, you can include it into your requirements.txt file.
+
+
+How to use
+----------
+Example for using the tool
+
+.. code-block:: shell
+
+	$ pip setup.py pyassembly
+
+
+How to get more options
+-----------------------
+After you installed the command you can get the list of options
+
+.. code-block:: shell
+
+	$ python setup.py pyassembly --help
+
+
+Reporting Issues
+----------------
+If you have suggestions, bugs or other issues with this command - please open an issue or make a pull request.
---
+++ pyassembly/main.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import shutil
+
+from distutils.cmd import Command
+from distutils.sysconfig import get_python_version
+
+from pip.commands import InstallCommand
+from pkg_resources import get_build_platform, Distribution
+
+
+class PyAssembly(Command):
+    description = "create an egg or zip distribution with all its transitive dependencies"
+    user_options = [
+        ('requirements-file=', 'r',
+         'install dependencies from the given requirements file. [default: requirements.txt]'),
+        ('destination-format=', 'f', 'assembly formats: zip or egg. [default: egg]'),
+        ('assembly-dir=', 'd', 'build the assembly into this dir. [default: pyassembly_dist]')]
+
+    boolean_options = ['keep-temp']
+
+    def initialize_options(self):
+        self.requirements_file = None
+        self.assembly_dir = None
+        self.destination_format = None
+
+    def finalize_options(self):
+        self.bdist_base = self.get_finalized_command('bdist').bdist_base
+        self.build_base = self.get_finalized_command('build').build_base
+        self.egg_info = self.get_finalized_command('egg_info').egg_info
+
+        if self.requirements_file is None:
+            self.requirements_file = 'requirements.txt'
+
+        if self.assembly_dir is None:
+            self.assembly_dir = 'pyassembly_dist'
+
+        if self.destination_format is None:
+            self.destination_format = 'egg'
+
+        if not self.destination_format == 'zip' and not self.destination_format == 'egg':
+            print('ERROR: destination-format can be egg or zip only')
+            sys.exit(1)
+
+    def run(self):
+        if os.path.exists(self.bdist_base):
+            shutil.rmtree(self.bdist_base)
+        os.makedirs(self.bdist_base)
+
+        if os.path.exists(self.assembly_dir):
+            shutil.rmtree(self.assembly_dir)
+
+        dist_dir = os.path.join(self.bdist_base, 'pyassembly')
+
+        # install deps, if needed
+        if os.path.exists(self.requirements_file):
+            install_command = InstallCommand(isolated=False)
+            install_command.main(args=['-r', self.requirements_file, '-t', dist_dir])
+
+        if self.destination_format == 'zip':
+            cmd = self.reinitialize_command('build')
+            cmd.build_purelib = dist_dir
+            self.run_command('build')
+
+            basename = Distribution(None, None, self.distribution.get_name(),
+                                    self.distribution.get_version(), get_python_version(),
+                                    self.distribution.has_ext_modules() and get_build_platform()).egg_name()
+            shutil.make_archive(base_name=os.path.join(self.assembly_dir, basename), format='zip', root_dir=dist_dir)
+
+        else:
+            bdist_egg = self.distribution.get_command_obj('bdist_egg')  # type: Command
+            bdist_egg.bdist_dir = dist_dir
+            bdist_egg.dist_dir = self.assembly_dir
+            bdist_egg.keep_temp = 1  # wtf?, it is True or False and not 1 or 0
+            bdist_egg.ensure_finalized()
+            bdist_egg.run()
+            shutil.rmtree(self.egg_info)
+
+        shutil.rmtree(self.build_base)
